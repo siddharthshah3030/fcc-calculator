2b56411b6e8c5534f8f30badcb3d822df38fabe9
Naveensundar <naveennvrgup@gmail.com>
2018-05-02 17:17:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+let out = "0";
+let display = $(".display");
+
+function update(temp) {
+  out = String(temp);
+  display.text(out);
+}
+
+$(".enter").click(() => {
+  let calculated;
+  
+  try {
+    calculated = eval(out).toFixed(3);
+  } catch (error) {
+    calculated = "error";
+  }
+
+  update(calculated);
+});
+
+$(".ac").click(() => {
+  update("0");
+});
+
+$(".c").click(() => {
+  if (out.length >1) {
+    update(out.slice(0, out.length-1));
+  } else {
+    update(0);
+  }
+});
+
+$(".math").click((e) => {
+  if (out == "0" || out == "error") {
+    update(e.target.innerText);
+  } else {
+    update(out + e.target.innerText);
+  }
+});
